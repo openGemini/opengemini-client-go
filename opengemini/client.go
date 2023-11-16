1edc4956a4170a56161a001b56c59f2f928b4f75
PennyYoon <55304072+Chenxulin97@users.noreply.github.com>
2023-11-16 22:31:33 +0800

    feat: support new OpenGemini client with config (#4)
    Signed-off-by: PennyYoon <525296438@qq.com>

--- opengemini/client.go
+++ opengemini/client.go
@@ -1,1 +1,45 @@
 package opengemini
+
+import (
+	"crypto/tls"
+)
+
+const (
+	AuthTypeToken AuthType = iota
+	AuthTypePassword
+)
+
+type Client interface {
+}
+
+type Config struct {
+	AddressList []*Address
+	AuthConfig  *AuthConfig
+	BatchConfig *BatchConfig
+	GzipEnabled bool
+	TlsConfig   *tls.Config
+}
+
+type Address struct {
+	Host string
+	Port int
+}
+
+type AuthType int
+
+type AuthConfig struct {
+	AuthType AuthType
+	Username string
+	Password string
+	Token    string
+}
+
+type BatchConfig struct {
+	BatchEnabled  bool
+	BatchInterval int
+	BatchSize     int
+}
+
+func NewClient(config *Config) (Client, error) {
+	return newClient(config)
+}
