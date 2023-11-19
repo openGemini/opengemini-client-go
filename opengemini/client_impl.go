251cb557efd61270d8bbb298065c501306d2dc40
yehao <944207160@qq.com>
2023-11-19 22:36:14 +0800

    feat: request set auth

--- opengemini/client_impl.go
+++ opengemini/client_impl.go
@@ -1,8 +1,11 @@
 package opengemini
 
 import (
+	"encoding/base64"
 	"errors"
+	"io"
 	"net/http"
+	"net/url"
 	"strconv"
 )
 
@@ -67,4 +70,88 @@ func newHttpClient(config Config) *http.Client {
 	} else {
 		return &http.Client{}
 	}
+}
+
+type requestMetadata struct {
+	queryValues   url.Values
+	header        http.Header
+	contentBody   io.Reader
+	contentLength int64
+}
+
+func (c *client) setAuthorization(method, url string, header http.Header) http.Header {
+	if header == nil {
+		header = make(http.Header)
+	}
+	if c.config.AuthConfig == nil {
+		return header
+	}
+
+	methods, ok := unAuthorization[url]
+	if ok {
+		for _, v := range methods {
+			if method == v {
+				return header
+			}
+		}
+	}
+
+	var authorization string
+	if c.config.AuthConfig.AuthType == AuthTypePassword {
+		encodeString := c.config.AuthConfig.Username + ":" + c.config.AuthConfig.Password
+		authorization = "Basic " + base64.StdEncoding.EncodeToString([]byte(encodeString))
+	}
+
+	header.Set("Authorization", authorization)
+	return header
+}
+
+func (c *client) requestByIdx(idx int, method, url string, metadata requestMetadata) (*http.Response, error) {
+	metadata.header = c.setAuthorization(method, url, metadata.header)
+	server := c.serverUrls[idx]
+	url = server + url
+	return c.executeMethod(method, url, metadata)
+}
+
+func (c *client) executeMethod(method, serverUrl string, metadata requestMetadata) (*http.Response, error) {
+	// default method: 'GET'.
+	if method == "" {
+		method = http.MethodGet
+	}
+
+	// set query values
+	if metadata.queryValues != nil {
+		u, err := url.Parse(serverUrl)
+		if err != nil {
+			return nil, err
+		}
+		u.RawQuery = metadata.queryValues.Encode()
+		serverUrl = u.String()
+	}
+
+	// Initialize a new HTTP request for the method.
+	req, err := http.NewRequest(method, serverUrl, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	// Set all headers.
+	for k, v := range metadata.header {
+		req.Header.Set(k, v[0])
+	}
+
+	// Set body
+	if metadata.contentLength == 0 {
+		req.Body = nil
+	} else {
+		req.Body = io.NopCloser(metadata.contentBody)
+		req.ContentLength = metadata.contentLength
+	}
+
+	res, err := c.cli.Do(req)
+	if err != nil {
+		return nil, err
+	}
+
+	return res, nil
 }
--- opengemini/url_const.go
+++ opengemini/url_const.go
@@ -1,3 +1,15 @@
 package opengemini
 
-const UrlPing = "/ping"
+import "net/http"
+
+const (
+	UrlPing   = "/ping"
+	UrlQuery  = "/query"
+	UrlStatus = "/status"
+)
+
+var unAuthorization = map[string][]string{
+	UrlPing:   {http.MethodHead, http.MethodGet},
+	UrlQuery:  {http.MethodOptions},
+	UrlStatus: {http.MethodHead, http.MethodGet},
+}
