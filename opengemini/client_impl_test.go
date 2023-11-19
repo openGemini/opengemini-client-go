1758bd8605548f29ff82ee46c481db761d7b090e
yehao <944207160@qq.com>
2023-11-19 23:03:20 +0800

    feat: request set auth
    Signed-off-by: yehao <944207160@qq.com>

--- opengemini/client_impl_test.go
+++ opengemini/client_impl_test.go
@@ -1,1 +1,29 @@
 package opengemini
+
+import (
+	"github.com/stretchr/testify/require"
+	"net/http"
+	"testing"
+)
+
+func TestSetAuthorization(t *testing.T) {
+	c := client{
+		config: &Config{
+			AuthConfig: &AuthConfig{
+				AuthType: AuthTypePassword,
+				Username: "test",
+				Password: "test pwd",
+			},
+		},
+	}
+
+	header := c.setAuthorization(http.MethodGet, UrlPing, nil)
+	require.Equal(t, "", header.Get("Authorization"))
+
+	header = c.setAuthorization(http.MethodOptions, UrlQuery, nil)
+	require.Equal(t, "", header.Get("Authorization"))
+
+	header = c.setAuthorization(http.MethodGet, UrlQuery, nil)
+	require.Equal(t, "Basic dGVzdDp0ZXN0IHB3ZA==", header.Get("Authorization"))
+
+}
