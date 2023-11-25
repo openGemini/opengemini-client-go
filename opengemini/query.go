03251f9e265f7f6d5b94a63d60fffb4b0da820e9
PennyYoon <55304072+Chenxulin97@users.noreply.github.com>
2023-11-25 23:41:02 +0800

    feat: support query method (#16)
    Signed-off-by: PennyYoon <525296438@qq.com>
    Co-authored-by: ZhangJian He <shoothzj@gmail.com>

---
+++ opengemini/query.go
@@ -0,0 +1,37 @@
+package opengemini
+
+import (
+	"encoding/json"
+	"io"
+)
+
+type Query struct {
+	Database        string
+	Command         string
+	RetentionPolicy string
+}
+
+// Query sends a command to the server
+func (c *client) Query(q Query) (*QueryResult, error) {
+	req := requestDetails{
+		queryValues: make(map[string][]string),
+	}
+	req.queryValues.Add("db", q.Database)
+	req.queryValues.Add("q", q.Command)
+	resp, err := c.executeHttpGet(UrlQuery, req)
+	if err != nil {
+		return nil, err
+	}
+
+	defer resp.Body.Close()
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+	var qr = new(QueryResult)
+	err = json.Unmarshal(body, qr)
+	if err != nil {
+		return nil, err
+	}
+	return qr, nil
+}
