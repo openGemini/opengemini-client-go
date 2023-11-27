696c8cef17932071b1d9ed558b7409f57131d1a1
cy <710058301@qq.com>
2023-11-28 00:43:41 +0800

    fix: query result member type fix (#19)
    Signed-off-by: cyruslo <710058301@qq.com>

--- opengemini/query_result.go
+++ opengemini/query_result.go
@@ -4,14 +4,14 @@ import "errors"
 
 // SeriesResult contains the results of a series query
 type SeriesResult struct {
-	Series []Series `json:"series,omitempty"`
-	Error  string   `json:"error,omitempty"`
+	Series []*Series `json:"series,omitempty"`
+	Error  string    `json:"error,omitempty"`
 }
 
 // QueryResult is the top-level struct
 type QueryResult struct {
-	Results []SeriesResult `json:"results,omitempty"`
-	Error   string         `json:"error,omitempty"`
+	Results []*SeriesResult `json:"results,omitempty"`
+	Error   string          `json:"error,omitempty"`
 }
 
 func (result *QueryResult) hasError() error {
