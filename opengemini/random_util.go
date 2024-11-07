aa47497cf5c35b7c077b17873999927601f22cae
ZhangJian He <shoothzj@gmail.com>
2024-11-07 14:10:11 +0800

    chore: update dependnecy
    Signed-off-by: ZhangJian He <shoothzj@gmail.com>

--- opengemini/random_util.go
+++ opengemini/random_util.go
@@ -13,32 +13,3 @@
 // limitations under the License.
 
 package opengemini
-
-import (
-	"crypto/rand"
-	"math/big"
-
-	"github.com/libgox/unicodex/letter"
-)
-
-func RandBytes(n int64) []byte {
-	if n <= 0 {
-		return []byte{}
-	}
-	b := make([]byte, n)
-	for i := range b {
-		index, err := rand.Int(rand.Reader, big.NewInt(int64(letter.EnglishCount)))
-		if err != nil {
-			panic(err)
-		}
-		b[i] = letter.EnglishLetters[index.Int64()]
-	}
-	return b
-}
-
-func RandStr(n int64) string {
-	if n <= 0 {
-		return ""
-	}
-	return string(RandBytes(n))
-}
